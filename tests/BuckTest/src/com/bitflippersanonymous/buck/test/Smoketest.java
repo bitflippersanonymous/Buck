88b1ef2cf1c2c3a81b3fab756033fa68fad00a22
bitflippersanonymous <ryan@bitflippersanonymous.com>
2012-09-13 12:02:15 -0700

    Move stuff

---
+++ tests/BuckTest/src/com/bitflippersanonymous/buck/test/Smoketest.java
@@ -0,0 +1,9 @@
+package com.bitflippersanonymous.buck.test;
+
+import junit.framework.TestCase;
+
+public class Smoketest extends TestCase {
+  public void testNonsense() {
+    assertTrue(1==1);
+  }
+}
